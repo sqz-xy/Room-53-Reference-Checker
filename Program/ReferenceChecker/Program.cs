9c6163acb555b8cb60ada4e72ec8acc879bbe39d
Tom Beet <tblakebeet@gmail.com>
2021-02-09 12:31:55 +0000

    Added Solution

---
+++ Program/ReferenceChecker/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReferenceChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
