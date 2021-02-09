f4b8d5cc64d5dee35da0e3a0cdad64b30d604e56
Tom Beet <tblakebeet@gmail.com>
2021-02-09 13:01:40 +0000

    Added Getters and Setters for Checker Class

--- Program/ReferenceChecker/Checker.cs
+++ Program/ReferenceChecker/Checker.cs
@@ -6,5 +6,60 @@ namespace ReferenceChecker
 {
     class Checker
     {
+        private string mInputString;
+        private int mKeyIndex = 0;
+
+        /*
+         * Constructor
+         */
+        public Checker()
+        { }
+
+        /*
+         * Getters and Setters for private variables
+         */
+        public string inputString
+        {
+            set
+            {
+                this.mInputString = value;
+            }
+            get
+            {
+                if(this.mInputString == "")
+                {
+                    throw new ArgumentNullException("Empty input detected");
+                }
+                else
+                {
+                    return this.mInputString;
+                }
+
+            }
+        }
+
+        public int KeyIndex
+        {
+            set
+            {
+                this.mKeyIndex = value;
+            }
+            get
+            {
+                if (this.mKeyIndex == -KeyIndex)
+                {
+                    throw new ArgumentNullException("Invalid Key");
+                }
+                else
+                {
+                    return this.mKeyIndex;
+                }
+            }
+        }
+
+        public void CheckReference()
+        {
+            Dictionary newDictionary = new Dictionary();
+        }
     }
 }
