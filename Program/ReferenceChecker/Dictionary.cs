41e50e6608af807d1725e8289209d917ba77a354
Tom Beet <tblakebeet@gmail.com>
2021-02-09 12:52:42 +0000

    Created dictionary class with 2 references to start off with

--- Program/ReferenceChecker/Dictionary.cs
+++ Program/ReferenceChecker/Dictionary.cs
@@ -6,5 +6,40 @@ namespace ReferenceChecker
 {
     class Dictionary
     {
+        private Dictionary<int, string> dReferences = new Dictionary<int, string>();
+
+        /*
+         * Getter Method for the dictionary
+         * Returns value with specified key
+         */
+        public string Get(int key)
+        {
+            string result = null;
+
+            if (dReferences.ContainsKey(key))
+            {
+                result = dReferences[key];
+            }
+
+            return result;
+        }
+        /*
+         * Initializes the dictionary
+         */
+        public void initialize()
+        {
+            SetDefaultReferences();
+        }
+
+        /*
+         * Sets the default references
+         * - Add functionality to set references later
+         * - Add more references later
+         */
+        private void SetDefaultReferences()
+        {
+            dReferences.Add('0', "gunga");
+            dReferences.Add('1', "ginga");
+        }
     }
 }
